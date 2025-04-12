6bd73d0396aad2dc5c90a49a275846d8c82c6be8
martijnbuitink <55871640+martijnbuitink@users.noreply.github.com>
2025-04-12 10:38:42 +0200

    Update map.js

--- map.js
+++ map.js
@@ -178,7 +178,7 @@ const mapPoints = [
     title: 'Geheime zender in De Wolkenkrabber (Victorieplein 45)',
     description: 'In september 1944 werd vanuit een appartement in het 12-verdiepingenhuis, beter bekend onder de bijnaam De Wolkenkrabber, radiocontact gelegd met een Brits verkenningsvliegtuig. Verzetsman Dijckmeester zond daar twee weken lang belangrijke berichten naar Londen. Terwijl verzetsman Sonderman met een uitkijkploeg de wacht hield op straat, werkte een radiotelegrafist vanuit de flat aan het Daniël Willinkplein. Toen de Duitsers de zender bijna opspoorden, wist het team op tijd te ontsnappen en verplaatste de zendpost zich via Amsterdam naar Aalsmeer, Purmerend en Monnickendam.',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/12-verdiepingenhuis_2019_%281%29.jpg/399px-12-verdiepingenhuis_2019_%281%29.jpg',
-    color: '#ff6600' // orange red
+    color: '#ffee8c' // pastel yellow
   },
   {
     lat: 52.366311,
@@ -186,7 +186,7 @@ const mapPoints = [
     title: 'De Carlton-crash',
     description: 'Als je nu over de Reguliersdwarsstraat loopt dan zie je op deze plek een gebouw staan wat niet bij de andere gebouwen in deze straat past. In de nacht van 26 op 27 april 1943 om 02:34 is hier een Engelse bommenwerper neergestort die door een Duits vliegtuig was neergehaald. Daarbij werd een reeks gebouwen verwoest aan de Reguliersdwarsstraat en het Singel, waaronder het hotelgebouw Carlton, waar op dat ogenblik een vestiging van de Duitse Luftwaffe gevestigd was. De zeven bemanningsleden van het toestel kwamen allen om, op de grond vielen zes burgerdoden en drie Duitse militaire slachtoffers. De brand die na het neerstorten ontstond geldt als een van de grootste Amsterdamse stadsbranden, een compleet blok tussen het Singel en de Reguliersdwarsstraat werd in de as gelegd.',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/HalifaxopCarlton1943.jpg/399px-HalifaxopCarlton1943.jpg',
-    color: '#ff6600' // orange red
+    color: '#ffee8c' // pastel yellow
   }
 ];
 
